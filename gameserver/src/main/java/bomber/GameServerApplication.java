0aad35fbc6d4058b0a867201fe0d85c431d115f9
Nikolaychik Aleksandr <aleks.nik.w@gmail.com>
2017-12-12 14:51:49 +0300

    web socket works now! changes location of some files

--- gameserver/src/main/java/bomber/GameServerApplication.java
+++ gameserver/src/main/java/bomber/GameServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GameServerApplication.class, args);
 	}
